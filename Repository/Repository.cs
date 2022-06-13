16e7ac80555d949dc249f8ab71bf7c1f08754d13
Duwaraga <18duwa@gmail.com>
2022-06-12 17:59:13 -0700

    created function in repository class to add new student

---
+++ Repository/Repository.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningManagement.Interface;
+using LearningManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningManagement.Repository
+{
+    public class Repository : IStudentRepository
+    {
+        private readonly AppDbContext _context;
+
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Student AddStudent(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return student;
+        }
+
+        
+    }
+}
