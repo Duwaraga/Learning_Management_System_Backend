40fe4407814158017f9985bdce24b3b06b6b193c
Duwaraga <18duwa@gmail.com>
2022-06-12 18:08:40 -0700

    Added http get request in controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LearningManagement.Interface;
+using LearningManagement.Repository;
+using LearningManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository _student;
+        
+        public StudentController(IStudentRepository student)
+        {
+            _student = student;
+        }
+        // GET: api/Student
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> List()
+        {
+            return await _student.GetStudents();
+        }
+        
+       
+
+    }
+}
