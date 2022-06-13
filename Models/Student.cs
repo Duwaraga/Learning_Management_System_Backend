13926487363ba223f5903e8ed2181c4fc93edb5b
Duwaraga <18duwa@gmail.com>
2022-06-12 17:47:05 -0700

    Added student domain class to add a table to the database

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LearningManagement.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string RegNo { get; set; }
+        
+        
+    }
+}
